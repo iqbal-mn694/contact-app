3d12278b32967904d76c3873d4f33fdfd5bbf534
iqbal-mn694 <garenahiber134@gmail.com>
2024-05-22 15:30:07 +0700

    make label and group endpoint for post and get

--- index.js
+++ index.js
@@ -1,9 +1,80 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send("list contact")
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+// use body parser middleware
+app.use(bodyParser.urlencoded( { extended: true}));
+app.use(bodyParser.json())
+
+// contact
+app.get('/', async (req, res) => {
+    // const contact = await prisma.contact.create({
+    //     data: {
+    //         name: "Iqbal",
+    //         number: 23311,
+    //         notes: "lorem ipsum",
+    //         profile_pict: "haha",
+    //         label: {
+    //             create: {
+    //                 label_name: "default"
+    //             }
+    //         }
+    //     }
+    // })
+    try {
+        const contact = await prisma.contact.findMany()
+        res.send(contact)
+    } catch (err) {
+        res.send(err)        
+    }
+})
+
+app.post('/add-contact', async (req, res) => {
+    try {
+        const contact = await prisma.contact.create({
+            data: {
+                name: req.body.name,
+                number: req.body.number,
+                notes: req.body.notes,
+                profile_pict: "haha.jpg",
+                label: {
+                    create: {
+                        label_name: "default"
+                    }
+                },
+                group: {
+                    create: {
+                        group_name: req.body.group
+                    }
+                }
+            }
+        })
+
+        res.send('Sukses Menambahkan Kontak')
+    } catch (err) {
+        res.send(req.body)
+    }
+
+})
+
+
+// label
+app.post('/contact/add-label', async (req, res) => {
+    try {
+        const label = await prisma.label.create({
+            data: {
+                label_name: req.body.label
+            }
+        })
+        res.send("Sukses Menambahkan Label")
+    } catch (err) {
+        res.send(err)        
+    }
 })
 
 app.listen(port, () => {
